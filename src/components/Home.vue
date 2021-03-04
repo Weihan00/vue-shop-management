5ad54c3e5096e8e4adc6ff64d34e9d3f68c36262
wpang <pangweihan12@gmail.com>
2021-03-04 20:11:49 +0100

    '完成用户列表功能的开发'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,154 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout"> 退出 </el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout"> 退出 </el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">| | |</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menuList"
+            :key="item.id"
+          >
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+          <el-main><router-view /></el-main>
+    </el-container>
+  </el-container>
+</template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧主体 -->
+      <el-main> <router-view></router-view> </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      menuList: [],
+      iconsObj: {
+        125: 'el-icon-user',
+        103: 'el-icon-camera-solid',
+        101: 'el-icon-s-data',
+        102: 'el-icon-d-caret',
+        145: 'el-icon-share',
+      },
+      isCollapse: false,
+      activePath: ""
+    }
+  },
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePaths')
+  },
   methods: {
     logout() {
       console.log(this)
       window.sessionStorage.clear()
       this.$router.push('login')
     },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      console.log(res)
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
+    },
   },
 }
 </script>
 
 <style scoped>
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #eaedf1;
+  font-size: 20px;
+}
+
+.el-header div {
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+}
+
+.el-header img {
+  width: 50px;
+  height: 50px;
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-aside .el-menu {
+  border-right: none;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.home-container {
+  height: 100%;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  text-align: center;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  cursor: pointers;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
-import {Message} from 'element-ui'
+import {Message, MessageBox} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/global.css'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
+axios.interceptors.request.use(config =>{
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
 // 这样每个vue的组件都可以直接通过this直接访问到$http
 Vue.prototype.$http = axios
 
@@ -16,6 +21,7 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
 // 之后在组件中就不用importt了，直接通过this.postRequest方法请求
 new Vue({
   router,
